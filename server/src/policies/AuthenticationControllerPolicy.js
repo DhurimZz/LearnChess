78f1732473affddbd1529ef25273c3a39c1c144f
DhurimZz <93375353+DhurimZz@users.noreply.github.com>
2023-01-06 14:16:21 +0100

    Fix problem with joi package
    Co-Authored-By: ArbnorLA <94640226+ArbnorLA@users.noreply.github.com>

--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -2,13 +2,11 @@ const Joi = require('joi')
 
 module.exports = {
     register (req, res, next) {
-        const schema = {
-          email: Joi.string().email(),
-          password: Joi.string().regex(
-            new RegExp('^[a-zA-Z0-9]{8,32}$')
-          )
-        }
-        const { error } = Joi.validate(req.body, schema)
+        const schema = Joi.object({
+            email: Joi.string().email(),
+            password: Joi.string().regex(/^[a-zA-Z0-9]{8,32}$/)
+          })
+          const { error } = schema.validate(req.body)
 
         if (error) {
             switch (error.details[0].context.key) {

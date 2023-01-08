8a6d715f9a5b3f801c18d94f0fbc56efa0c53439
DhurimZz <93375353+DhurimZz@users.noreply.github.com>
2023-01-08 21:44:04 +0100

    Install vuetify
    Co-Authored-By: ArbnorLA <94640226+ArbnorLA@users.noreply.github.com>

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,20 +1,16 @@
 <template>
   <v-app>
-    <div>
-      <v-img
-    lazy-src="https://picsum.photos/id/11/10/6"
-    max-height="150"
-    max-width="250"
-    src="https://picsum.photos/id/11/500/300"
-    ></v-img>
-    </div>
+    <v-main>
+      <v-container>
+        <div>
+          <input type="email" name="email" v-model="email" placeholder="email" /> <br />
+          <input type="password" name="password" v-model="password" placeholder="password" /> <br />
+          <span class="error" v-html="error"></span>
+          <button @click="register">Register</button>
+        </div>
+      </v-container>
+    </v-main>
   </v-app>
-  <!-- <div>
-    <input type="email" name="email" v-model="email" placeholder="email" /> <br />
-    <input type="password" name="password" v-model="password" placeholder="password" /> <br />
-    <span class="error" v-html="error"></span>
-    <button @click="register">Register</button>
-  </div> -->
 </template>
 <script>
 import AuthenticationService from '@/services/AuthenticationService'

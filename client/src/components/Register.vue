c2cf5af7ba0fa6d47f795e706a1dc6c15d4fb68a
DhurimZz <93375353+DhurimZz@users.noreply.github.com>
2023-01-08 11:40:29 +0100

    Fix some bugs
    Co-Authored-By: ArbnorLA <94640226+ArbnorLA@users.noreply.github.com>

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,26 +1,41 @@
 <template>
-  <div>
+  <v-app>
+    <div>
+      <v-img
+    lazy-src="https://picsum.photos/id/11/10/6"
+    max-height="150"
+    max-width="250"
+    src="https://picsum.photos/id/11/500/300"
+    ></v-img>
+    </div>
+  </v-app>
+  <!-- <div>
     <input type="email" name="email" v-model="email" placeholder="email" /> <br />
     <input type="password" name="password" v-model="password" placeholder="password" /> <br />
+    <span class="error" v-html="error"></span>
     <button @click="register">Register</button>
-  </div>
+  </div> -->
 </template>
-
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -29,4 +44,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error {
+  color: red;
+}
 </style>

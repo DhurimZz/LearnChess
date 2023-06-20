d59041346a448c37e35b693f4abebaaaf0ab7d17
38346 <38346@DESKTOP-QK8141T>
2023-06-20 08:24:54 +0200

    ..

--- client/src/components/AboutUs.vue
+++ client/src/components/AboutUs.vue
@@ -35,9 +35,8 @@
                 <v-col cols="12" md="6">
                     <p>
                         Arbnor, Dhurim and Argjenta, we are three students of computer engineering, sharing a passion for chess and a
-                        desire to
-                        apply our programming skills to create something innovative and exciting. We decided to start
-                        working on a web application that would provide a unique chess experience for players of all levels.
+                        desire to apply our programming skills to create something innovative and exciting. 
+                        We decided to start working on a web application that would provide a unique chess experience for players of all levels.
                     </p>
                     <blockquote class="text-h4 pa-5 my-4 font-weight-thin">
                         Personally, I rather look forward to a computer program winning the world chess championship.
